dd53c4cf8e976ab4e711d7130fda5c00caa27f39
lishuai <244821338@qq.com>
2022-10-09 14:50:24 +0800

    test

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import PrismaModule from './data/prisma.module';
 @Module({
-  imports: [],
+  imports: [PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
